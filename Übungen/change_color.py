643e62335d2b37cce2be5f94dcfb07d90f52dbab
Marco Savoca <109957478+masa-gymmich@users.noreply.github.com>
2023-04-27 19:45:21 +0200

    Update change_color.py

--- Übungen/change_color.py
+++ Übungen/change_color.py
@@ -6,7 +6,7 @@ HEIGHT = 200
 ball_radius = 50
 ball_color = "red"
 
-# Handler für keydown 
+# Button handler
 def keydown(key):
   pass
 
@@ -21,8 +21,8 @@ def draw(canvas):
 # Erzeuge frame und registriere event handler
 
 frame = simplegui.create_frame("Change Color", 300, 200)
-frame.set_keydown_handler(keydown)
 frame.set_draw_handler(draw)
+# Füge hier noch die buttons "red" und 
 
 # Starte die Frame-Animation
 frame.start()
