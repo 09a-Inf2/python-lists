d840bfc23fa26a22736e2c312099f43fd0cce8b3
Marco Savoca <109957478+masa-gymmich@users.noreply.github.com>
2023-05-11 21:56:33 +0200

    Update velocity_control_accelleration.py

--- velocity_control_accelleration.py
+++ velocity_control_accelleration.py
@@ -14,22 +14,14 @@ vel = [0, 0]
 # define event handlers
 def draw(canvas):
     # Update ball position
-    ball_pos[0] += vel[0]
-    ball_pos[1] += vel[1]
+   
 
     # Draw ball
     canvas.draw_circle(ball_pos, BALL_RADIUS, 2, "Red", "White")
 
 def keydown(key):
     acc = 1
-    if key==simplegui.KEY_MAP["left"]:
-        vel[0] -= acc
-    elif key==simplegui.KEY_MAP["right"]:
-        vel[0] += acc
-    elif key==simplegui.KEY_MAP["down"]:
-        vel[1] += acc
-    elif key==simplegui.KEY_MAP["up"]:
-        vel[1] -= acc
+    # velocity update here
         
     print (ball_pos)
     
